7210692ec480a5b8a6eb8623fb596ae45eb3d1c1
togswr <tatsuya.ogswr@gmail.com>
2022-08-04 20:19:40 +0900

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { scale } from "svelte/transition";
+
+  let dodosuko = [];
+
+  const interval = setInterval(() => {
+    dodosuko.push(getRandomInt(0, 2));
+    const check = dodosuko.slice(dodosuko.length - 4 * 3, dodosuko.length);
+    if (check.join("") === "011101110111") {
+      dodosuko.push(2);
+      clearInterval(interval);
+    }
+    dodosuko = dodosuko;
+  }, 1);
+
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+</script>
+
+<main>
+  {#each dodosuko as v}
+    {#if v === 0}<span in:scale>ドド</span>{/if}
+    {#if v === 1}<span in:scale>スコ</span>{/if}
+    {#if v === 2}<span in:scale class="love">ラブ注入♡</span>{/if}
+  {/each}
+</main>
+
+<style>
+  main {
+    margin-bottom: 20vh;
+  }
+  .love {
+    color: hotpink;
+    font-weight: bold;
+  }
+</style>
